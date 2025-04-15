4709c3e2b82cb2c6eaf11e13eef45d0b0f38a581
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-15 02:51:24 -0300

    feat(TransacaoDeleteController): @Slf4j loggers

---
+++ src/main/java/br/com/jpaveiro/itau99backendchallenge/controllers/transacao/TransacaoDeleteController.java
@@ -0,0 +1,29 @@
+package br.com.jpaveiro.itau99backendchallenge.controllers.transacao;
+
+import br.com.jpaveiro.itau99backendchallenge.services.transacao.TransacaoDeleteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TransacaoDeleteController {
+    private final TransacaoDeleteService service;
+
+    public TransacaoDeleteController(final TransacaoDeleteService service)
+    {
+        this.service = service;
+    }
+
+    @DeleteMapping("/transacao")
+    public ResponseEntity<Void> performar()
+    {
+        log.info("DELETE /transacao - Início do processamento.");
+        service.executar();
+        log.info("DELETE /transacao - Processamento finalizado.");
+        return ResponseEntity
+                .status(200)
+                .build();
+    }
+}
