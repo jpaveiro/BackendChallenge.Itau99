b048c549b6793626bcae17ff995ad9c4a1f278d2
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-15 02:48:48 -0300

    refactor: renamed PostTransacaoDTO to TransacaoPostDTO

--- src/main/java/br/com/jpaveiro/itau99backendchallenge/dto/transacao/PostTransacaoDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.jpaveiro.itau99backendchallenge.dto.transacao;
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-
-public record PostTransacaoDTO(BigDecimal valor, OffsetDateTime dataHora) {
-
-}
