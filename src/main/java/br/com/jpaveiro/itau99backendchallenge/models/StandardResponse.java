bd66e4cc54cb7dea6d2e41db998ebe64c8a3deb4
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-15 02:53:22 -0300

    feat: added StandardResponse class

---
+++ src/main/java/br/com/jpaveiro/itau99backendchallenge/models/StandardResponse.java
@@ -0,0 +1,21 @@
+package br.com.jpaveiro.itau99backendchallenge.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StandardResponse {
+    private final int statusCode;
+    private final String mensagem;
+
+    public StandardResponse(final int statusCode) {
+        this.statusCode = statusCode;
+        this.mensagem = "";
+    }
+
+    public StandardResponse(final int statusCode, final String mensagem) {
+        this.statusCode = statusCode;
+        this.mensagem = mensagem;
+    }
+}
