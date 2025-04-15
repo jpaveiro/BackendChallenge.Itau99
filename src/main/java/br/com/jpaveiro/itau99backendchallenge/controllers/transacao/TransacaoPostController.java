1457069867f0760b9965a3e4583cc059052e99ba
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-15 02:48:24 -0300

    refactor: renamed PostTransacaoController to TransacaoPostController

---
+++ src/main/java/br/com/jpaveiro/itau99backendchallenge/controllers/transacao/TransacaoPostController.java
@@ -0,0 +1,38 @@
+package br.com.jpaveiro.itau99backendchallenge.controllers.transacao;
+
+import br.com.jpaveiro.itau99backendchallenge.dto.transacao.TransacaoPostDTO;
+import br.com.jpaveiro.itau99backendchallenge.services.transacao.TransacaoPostService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TransacaoPostController {
+    private final TransacaoPostService service;
+
+    public TransacaoPostController(final TransacaoPostService service)
+    {
+        this.service = service;
+    }
+
+    @PostMapping("/transacao")
+    public ResponseEntity<Void> performar(@RequestBody final TransacaoPostDTO dto) {
+        log.info("POST /transacao - Início do processamento.");
+        var resposta = service.executar(dto);
+
+        if (resposta.getStatusCode() == 422) {
+            log.error("POST /transacao - {}", resposta.getMensagem());
+            return ResponseEntity
+                    .status(resposta.getStatusCode())
+                    .build();
+        }
+
+        log.info("DELETE /transacao - Processamento finalizado.");
+        return ResponseEntity
+                .status(201)
+                .build();
+    }
+}
