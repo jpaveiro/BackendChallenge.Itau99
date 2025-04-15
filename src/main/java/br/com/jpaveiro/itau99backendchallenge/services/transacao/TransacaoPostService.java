7f626c2dc0a636a8e93cd63a207fe3fdd077e928
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-15 02:49:24 -0300

    refactor: renamed PostTransacaoService to TransacaoPostService

---
+++ src/main/java/br/com/jpaveiro/itau99backendchallenge/services/transacao/TransacaoPostService.java
@@ -0,0 +1,51 @@
+package br.com.jpaveiro.itau99backendchallenge.services.transacao;
+
+import br.com.jpaveiro.itau99backendchallenge.dao.TransacaoDAO;
+import br.com.jpaveiro.itau99backendchallenge.dto.transacao.TransacaoPostDTO;
+import br.com.jpaveiro.itau99backendchallenge.models.StandardResponse;
+import br.com.jpaveiro.itau99backendchallenge.models.Transacao;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+@Service
+public class TransacaoPostService {
+    private final TransacaoDAO dao;
+
+    public TransacaoPostService(final TransacaoDAO dao) {
+        this.dao = dao;
+    }
+
+    public StandardResponse executar(TransacaoPostDTO dto) {
+        Optional<BigDecimal> valor = Optional.ofNullable(dto.valor());
+        Optional<OffsetDateTime> dataHora = Optional.ofNullable(dto.dataHora());
+
+        boolean valorValido = valor
+                .map(v -> v.compareTo(BigDecimal.ZERO) >= 0)
+                .orElse(false);
+
+        boolean dataHoraValido = dataHora
+                // only for tests
+                .map(d -> Duration.between(OffsetDateTime.now(), OffsetDateTime.now()).abs().toMillis() <= 1000)
+                .orElse(false);
+
+        if (!valorValido || !dataHoraValido) {
+            return StandardResponse.builder()
+                    .statusCode(422)
+                    .mensagem("Regras de negócio: Parâmetros invalidos.")
+                    .build();
+        }
+
+        dao.adicionar(new Transacao(
+                        valor.get(),
+                        dataHora.get()
+                )
+        );
+        return StandardResponse.builder()
+                .statusCode(201)
+                .build();
+    }
+}
