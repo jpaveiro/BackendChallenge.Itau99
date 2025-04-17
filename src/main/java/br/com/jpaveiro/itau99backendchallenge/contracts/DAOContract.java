14b8f7b4d036ced5d86b2c6fa635462c4293ecb0
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-04-17 02:41:35 -0300

    feat(dao): implement listarTodosQueList method in DAOContract interface

--- src/main/java/br/com/jpaveiro/itau99backendchallenge/contracts/DAOContract.java
+++ src/main/java/br/com/jpaveiro/itau99backendchallenge/contracts/DAOContract.java
@@ -1,5 +1,6 @@
 package br.com.jpaveiro.itau99backendchallenge.contracts;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface DAOContract<M> {
@@ -7,6 +8,7 @@ public interface DAOContract<M> {
     void apagarTodos();
     Stream<M> listarTodos();
     Stream<M> listarTodosQue(Condicao<M> condicao);
+    List<M> listarTodosQueList(Condicao<M> condicao);
 
     @FunctionalInterface
     interface Condicao<M> {
